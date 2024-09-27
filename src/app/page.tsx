fc72a0daf7b754ce093e9b7c33da2c79a6c340d7
simonlearnscoding <40177876+simonlearnscoding@users.noreply.github.com>
2024-09-28 00:09:16 +0200

    header halfway there

---
+++ src/app/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import { Bebas_Neue } from "@next/font/google";
+import { Globe } from "@phosphor-icons/react/dist/ssr";
+import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
+
+const bebasNeue = Bebas_Neue({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const Header = () => (
+  <div className="flex flex-col">
+    <div
+      className={`${bebasNeue.className} text-8xl max-w-2xl flex mt-4 leading-[5.5rem] text-zinc-100`}
+    >
+      Uniting BRANDS with their VISIONS
+    </div>
+    <div className="font-sans text-zinc-100 text-xl mt-3 leading-7 font-light  max-w-sm ">
+      {" "}
+      WE BELIEVE THAT THOSE WHO DARE WILL FIND THEIR PATH
+    </div>
+  </div>
+);
+
+const Navbar = () => (
+  <div className="py-6 flex my-4 w-full">
+    <div className="text-sans text-xl w-full h-full font-regular text-zinc-50">
+      LOGO
+    </div>
+    <div className="flex-grow flex items-center gap-2 text-zinc-100 ml-auto">
+      <Globe size={20} />
+      <div className="whitespace-nowrap font-sans text-base font-regular">
+        START YOUR JOURNEY
+      </div>
+      <ArrowRight size={20} />
+    </div>
+  </div>
+);
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen relative">
+      {/* Background Overlay */}
+      <Image
+        alt={"noise"}
+        src={"/noise.png"}
+        fill
+        className="absolute inset-0  opacity-70 z-0 pointer-events-none"
+      />
+
+      {/* Content */}
+      <div className="flex flex-col w-full h-full bg-zinc-900 bg-noise-texture bg-cover bg-center bg-blend-overlay px-20 z-10">
+        <Navbar />
+        <Header />
+        <div className="absolute bottom-[-112px] right-0">
+          <Image src={"/meteor.png"} alt={"meteor"} width={600} height={600} />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ["Bebas Neue", "cursive"], // Ensure fallback font is included
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
