6b843677ede6c68eec572655523dea7a42835fc3
simonlearnscoding <simon.muscas@gmail.com>
2024-10-24 09:11:20 +0200

    changing ts

--- src/app/modules/Team/index.tsx
+++ src/app/modules/Team/index.tsx
@@ -27,17 +27,19 @@ type MemberProps = {
 };
 const TeamMember = ({ data }: { data: MemberProps }) => {
   return (
-    <div className="flex group flex-col relative flex-1 items-center h-[663px] w-full">
-      {/* Image that remains visible at all times */}
-      <Image
-        src={data.image} // Use data.image for the image source
-        alt={data.name}
-        fill
-        className="object-cover rounded-3xl"
-      />
+    <div className="flex group flex-col p-[1px] bg-zinc-300 hover:bg-gradient-to-b hover:from-zinc-300/80 hover:to-primary-500 rounded-3xl relative flex-1 items-center h-[663px] w-full">
+      {/* Wrapper with a border */}
+      <div className="relative h-full w-full rounded-3xl  ">
+        <Image
+          src={data.image}
+          alt={data.name}
+          fill
+          className="object-cover rounded-3xl" // Keep this for the image, no need to add a border here
+        />
+      </div>
 
       {/* Overlay with transition on hover */}
-      <div className="absolute inset-0 flex justify-center items-center transition-all duration-500 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-5 w-full h-full rounded-3xl bg-gradient-to-t to-black/0 from-blue-500/75 z-10">
+      <div className="absolute inset-0 flex justify-center items-center transition-all duration-500 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-2 w-full h-full rounded-3xl bg-gradient-to-t to-black/0 from-primary-500/75 z-10">
         <div className="flex flex-col mt-auto px-7 pb-9 justify-end">
           <div className="font-bebas text-6xl text-white transition-all duration-500 ease-in-out">
             {data.name}
