52a54ef1e97d632c67a0d58868a7b7d2f824753f
simonlearnscoding <simon.muscas@gmail.com>
2024-10-25 22:04:51 +0200

    changed texst

--- src/app/modules/Team/index.tsx
+++ src/app/modules/Team/index.tsx
@@ -58,15 +58,14 @@ const TeamMembers = [
     role: "DEV",
     image: "/Team-simon.png",
     description: `
-lorem ipsum dolor sit amet consectetur. Condimentum congue penatibus fusce imperdiet quam ut. Amet tempor enim ultrices magna porttitor urna. A quisque et etiam morbi lectus eros sed diam quis. Integer pellentesque diam eu mollis nibh morbi.
-    `,
+We customize your website for optimal performance that goes beyond a typical design agency can offer. No templates or pre-built solution but instead a proper tailor-made website that will help you achieve business needs and success. From complex features to integrated third-party systems that’s a smooth user experience that will have more customers staying in your website creating more conversion opportunities    `,
   },
   {
     name: "Miracle",
     role: "DESIGN",
     image: "/Team-miracle.png",
     description: `
-lorem ipsum dolor sit amet consectetur. Condimentum congue penatibus fusce imperdiet quam ut. Amet tempor enim ultrices magna porttitor urna. A quisque et etiam morbi lectus eros sed diam quis. Integer pellentesque diam eu mollis nibh morbi.
+We here at _(insert company name) create intelligent websites that aren’t hiding from the world to see. This is done by creating a website that will have organic traffic and search engine ranking so people will can actually find it. A competitor analysis will be done and your SEO campaign will be monitored so the website we create will be competitive in it’s presentation using the most optimized keywords and Google Analytics. 
     `,
   },
   {
@@ -74,8 +73,7 @@ lorem ipsum dolor sit amet consectetur. Condimentum congue penatibus fusce imper
     role: "MARKETING",
     image: "/Team-james.png",
     description: `
-lorem ipsum dolor sit amet consectetur. Condimentum congue penatibus fusce imperdiet quam ut. Amet tempor enim ultrices magna porttitor urna. A quisque et etiam morbi lectus eros sed diam quis. Integer pellentesque diam eu mollis nibh morbi.
-    `,
+I will help you stay in touch with your users expectations of your product. I deliver user-centric solutions by exploring what your users want and need, I create designs that are not only beautiful to look at, but also meaningful and pleasant to interact with.     `,
   },
 ];
 const TeamArea = () => {
