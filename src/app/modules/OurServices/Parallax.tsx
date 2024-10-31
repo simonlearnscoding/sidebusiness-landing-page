7fea7e64227cddc06425f1cc7fa88f30013ea1af
Simon M <simon.muscas@gmail.com>
2024-10-31 16:45:55 +0100

    header design

--- src/app/modules/OurServices/Parallax.tsx
+++ src/app/modules/OurServices/Parallax.tsx
@@ -1,6 +1,7 @@
 import useSectionOnScroll from "@/hooks/useSectionOnScroll";
 import useSectionObserver from "@/hooks/useSectionObserver";
 import { ReactNode } from "react";
+import Image from "next/image";
 
 const Section = ({
   section,
@@ -35,14 +36,29 @@ export default function ParallaxComponent() {
     <div className="min-h-screen">
       <Section section="first">
         <Container bgColor="white">
-          <h1 className="text-zinc-600 w-8/12">
-            WE ARE Uniting BRANDS with their VISION
-          </h1>
+          <div className="flex flex-col">
+            <h1 className="text-zinc-500 w-11/12 text-[200px]">
+              WE ARE Uniting <span className="text-primary-500">BRANDS</span>{" "}
+              with their <span className="text-primary-500">VISION</span>
+            </h1>
+            <div className="font-sans text-zinc-700 max-w-md mt-10  text-xl">
+              These ideas are what have made the beautiful world we have today.
+            </div>
+
+            <div className="flex justify-start mt-10">
+              <div className="bg-primary-500 text-xl font-sans text-white py-4 px-5">
+                START YOUR JOURNEY
+              </div>
+            </div>
+            <div className="absolute w-96 right-80 h-96 bottom-0">
+              <Image src="/HandArrow.svg" fill alt="Hand Arrow" />
+            </div>
+          </div>
         </Container>
       </Section>
       <Section section="second">
         <Container bgColor="primary-500">
-          <h1>
+          <h1 className="w-8/12">
             Everything we see, experience, and benefit today is with thought the
             power of ideas.{" "}
           </h1>
