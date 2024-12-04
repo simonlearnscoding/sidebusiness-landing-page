66d058e3bc01f446e62e4d2154002e93435b85e3
Simon M <simon.muscas@gmail.com>
2024-12-04 17:51:35 +0100

    making it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useScrollAndMousePosition from "@/hooks/useScrollAndMousePos";
 import Navbar from "./components/Navbar";
-import { Code } from "@phosphor-icons/react";
+import { Code, ArrowRight, TrendUp, PenNib } from "@phosphor-icons/react";
 // import ContactFormSection from "./modules/ContactForm";
 // import HeaderSection from "./modules/Header";
 import MobileCTA from "./components/MobileCTA";
@@ -42,12 +42,12 @@ export default function Home() {
 
 function Header() {
   return (
-    <div className="flex flex-col gap-10 mx-4 mt-28">
+    <div className="flex  flex-col gap-10 lg:gap-2 h-fit mx-4 lg:mx-20 mt-28">
       <h1 className="flex  flex-col">YOUR VISION, OUR MISSION</h1>
-      <p className="flex text-2xl font-sans  w-9/12 flex-col">
+      <p className="flex md:text-3xl text-2xl font-sans  w-9/12 flex-col">
         At Zuesite We Make Your Vision Into Reality
       </p>
-      <button className="bg-white font-sans text-2xl text-black py-4 px-5 ">
+      <button className="bg-white font-sans lg:mr-auto text-2xl lg:text-3xl lg:mt-24 text-black py-4 lg:py-5 px-5 lg:px-10">
         START YOUR JOURNEY
       </button>
     </div>
@@ -56,37 +56,65 @@ function Header() {
 
 function SubHeader() {
   return (
-    <div className=" w-full my-10">
-      <div className="mx-4  rounded-lg bg-gray-800 h-96"> </div>
+    <div className=" w-full my-10  lg:my-36 h-fit">
+      <div className="mx-4 lg:mx-20 rounded-lg bg-gray-800 h-96 lg:h-[737px]">
+        {" "}
+      </div>
     </div>
   );
 }
 
 function Explanation() {
   return (
-    <div className="flex mx-4 gap-10 flex-col mb-16 ">
-      <h1 className="flex  flex-col">
+    <div className="flex mx-4 lg:mx-20 min-h-screen  justify-center  flex-col mb-16 lg:mb-32">
+      <h2 className="flex  flex-col lg:w-8/12 mb-6 mt-4">
         we take the stress off of website building
-      </h1>
-      <p className="flex  font-sans   flex-col">
-        With an exceptional team of experts in website building, we will listen
-        and implement every detail you have for your dream website. We don't
-        force a cookie-cutter one size fits all template that lacks originality
-        or any unique qualities that you get from Wix, Wordpress, and other page
-        builders.
-      </p>
+      </h2>
+      <div className="flex">
+        <div className="flex gap-10 lg:gap-0  flex-1 flex-col">
+          <p className="flex  font-sans   ">
+            With an exceptional team of experts in website building, we will
+            listen and implement every detail you have for your dream website.
+            We don't force a cookie-cutter one size fits all template that lacks
+            originality or any unique qualities that you get from Wix,
+            Wordpress, and other page builders.
+          </p>
+          <div className="bg-gray-400 h-[297px] lg:hidden w-full"> </div>
+
+          <p className="flex  font-sans   flex-col">
+            You also won't be working alone, by yourself with an A.I. computer
+            giving you generic suggestions on what would "improve" your website.
+            Instead you be a part of a team that's cares about your success, who
+            will work together every step of the way that isn't there just to
+            collect a subscription purchase.
+          </p>
+          <div className="flex lg:gap-6 gap-1 lg:mt-16 mt-2 md:mt-6 mb-4 ">
+            <div className=" flex justify-center items-center bg-zinc-800 p-5 rounded-full border-solid border-white/5 border-[2px]">
+              <ArrowRight className="-rotate-45 lg:w-10 w-6 h-6 lg:h-10" />
+            </div>
+
+            <div className="font-sans lg:text-4xl  text-2xl md:text-3xl my-auto ml-4 text-white">
+              {" "}
+              LETS PUT YOUR BUSINESS ON THE MAP{" "}
+            </div>
+          </div>
+        </div>
+        <div className="justify-end items-center  hidden xl:flex flex-1  h-fill">
+          <div className="h-full w-[425px]  xl:mr-24 bg-gray-300"> </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-function Service({ name, bullets }) {
+function Service({ Icon, name, bullets }) {
   return (
-    <div className="flex px-3  border-solid border-[1px]  border-white py-10 flex-col">
-      <div className="flex items-center gap-2 ">
-        <Code size={32} />
-        <h2 className="   ">{name}</h2>
+    <div className="flex px-3 lg:flex-1 h-full  xl:pl-8 xl:pt-10  xl:pb-20 border-solid border-[1px]  border-white py-10  flex-col">
+      <div className="flex lg:flex-col  items-start gap-2 lg:gap-6 ">
+        <Icon size={32} />
+        <h2 className="md:text-6xl xl:text-7xl">{name}</h2>
       </div>
-      <ul className=" list-disc pl-8 mt-4">
+      <ul className=" list-disc pl-8 mt-4 lg:mt-6">
         {bullets.map((bullet, index) => (
           <li key={index} className="font-sans text-2xl">
             {" "}
@@ -99,15 +127,21 @@ function Service({ name, bullets }) {
 }
 function BookAnAppointment() {
   return (
-    <div className="flex my-12 px-4  flex-col ">
-      <h2 className="flex mb-3   flex-col">
-        Book an appointment for a free 45 min consultation call to discuss your
-        dream website.
-      </h2>
-      <p className="mb-6">Let’s put your business on the map!</p>
-      <button className="bg-white font-sans text-2xl text-black py-4 px-5 ">
-        SAVE A SPOT
-      </button>
+    <div className="flex min-h-screen my-12 gap-12   lg:items-center lg:flex-row-reverse flex-col ">
+      <div className="bg-zinc-600 border-solid border-[2px] border-zinc-300 w-full h-96  lg:h-full">
+        {" "}
+      </div>
+
+      <div className="mx-4 lg:mx-20">
+        <h2 className="flex mb-3   w-11/12 flex-col">
+          Book an appointment for a free 45 min consultation call to discuss
+          your dream website.
+        </h2>
+        <p className="mb-6">Let’s put your business on the map!</p>
+        <button className="bg-white font-sans text-2xl text-black py-4 px-5 ">
+          SAVE A SPOT
+        </button>
+      </div>
     </div>
   );
 }
@@ -115,19 +149,8 @@ function BookAnAppointment() {
 function Services() {
   const services = [
     {
-      name: "Web Development",
-      bullets: [
-        "Custom Websites",
-        "E-commerce",
-        "Blog",
-        "Portfolio",
-        "Landing Page",
-        "Web Application",
-      ],
-    },
-
-    {
       name: "Code",
+      icon: Code,
       bullets: [
         "Custom Websites",
         "E-commerce",
@@ -140,6 +163,21 @@ function Services() {
 
     {
       name: "Design",
+      icon: PenNib,
+      bullets: [
+        "Custom Websites",
+        "E-commerce",
+        "Blog",
+        "Portfolio",
+        "Landing Page",
+        "Web Application",
+      ],
+    },
+
+    {
+      name: "Marketing",
+
+      icon: TrendUp,
       bullets: [
         "Custom Websites",
         "E-commerce",
@@ -151,14 +189,18 @@ function Services() {
     },
   ];
   return (
-    <div className="flex bg-zinc-800 border-solid  flex-col">
-      <div className="py-4 px-4   border-solid border-[1px] border-white ">
+    <div className="flex  border-solid min-h-screen  flex-col">
+      <div className="py-4 px-4  bg-zinc-800 lg:px-20   border-solid border-[1px] border-white ">
         <h1 className=" w-full  ">OUR SERVICES</h1>
       </div>
 
-      <div className=" w-full px-4 flex flex-col gap-4 py-8">
+      <div className=" w-full px-4 flex flex-col  h-full lg:px-20 lg:flex-row gap-4 py-8 mt-4 lg:mt-20">
         {services.map((service) => (
-          <Service name={service.name} bullets={service.bullets} />
+          <Service
+            Icon={service.icon}
+            name={service.name}
+            bullets={service.bullets}
+          />
         ))}
       </div>
     </div>
