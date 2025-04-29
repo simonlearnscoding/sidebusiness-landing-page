8c7de368530b8fe52d6d1dc9fd9cd2030e2f92ad
simonlearnscoding <simon.muscas@gmail.com>
2025-04-29 23:24:54 +0200

    rm cmt

--- src/app/components/SubHeader.tsx
+++ src/app/components/SubHeader.tsx
@@ -8,12 +8,9 @@ export default function SubHeader() {
   const blurValue = Math.min(roundedScrollPos, 20);
 
   return (
-    <section className="w-full mt-6 overflow-hidden">
-      {" "}
-      {/* Added overflow-hidden */}
-      {/* Main container with dynamic blur */}
+    <section className="w-full mt-24 overflow-hidden">
       <div
-        className="left-0 z-0 h-48 lg:h-[480px] w-full"
+        className="left-0 z-0 h-96 lg:h-[480px] w-full"
         style={{
           filter: `blur(${blurValue}px)`,
           transform: `translateY(${scrollPos / 2}px)`,
