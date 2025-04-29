0544e26539b5c6acf7cb3f37c5e3e11bfd03ee65
simonlearnscoding <simon.muscas@gmail.com>
2025-04-29 22:40:16 +0200

    fix SSR issue

--- src/app/components/SubHeader.tsx
+++ src/app/components/SubHeader.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect } from "react";
 import useMousePositionStore from "@/store/useMousePositionStore";
 import RotatingShape from "./RotatingShape";
 
--- src/hooks/useSectionOnScroll.ts
+++ src/hooks/useSectionOnScroll.ts
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 import { useSectionStore } from "@/store/useSectionStore";
 
